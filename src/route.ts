29ef7d50391f41cdb4a64c136b6fd62021a2945b
DhruvGahoi <dhruvgahoi11111@gmail.com>
2026-03-14 19:13:01 +0530

    chore: removed the todos

--- src/route.ts
+++ src/route.ts
@@ -6,8 +6,3 @@ const router = Router();
 router.post("/identify", identifyContact);
 
 export { router };
-
-
-// TODO :
-// Add auth as middleware : brownie
-// const data = await identify(email ?? null, phoneNumber ?? null, client); client should not be passed. null check for email and phone nukber
